d42f60458b223aa4e04628cb27f8a7f539c4ef62
BenjaTorrealba <benjamintorrealbacarreno@gmail.com>
2024-05-16 21:35:52 -0400

    asd

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const cors = require("cors"); 
 const app = express();
+const productos = require("./productos.json");
+
+app.use(cors()); 
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.get("/productos", (req, res) => {
+    res.json(productos);
+    });
+
+
+app.listen(3002, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
