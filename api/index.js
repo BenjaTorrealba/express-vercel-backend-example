6a6342794a86c2af4e517a0fd7a7eaef7c2775b9
BenjaTorrealba <benjamintorrealbacarreno@gmail.com>
2024-07-04 20:29:17 -0400

    agregue dias

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors"); 
 const app = express();
 const productos = require("./productos.json");
-
+const dias = require("./dias.json");
 app.use(cors()); 
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
@@ -10,7 +10,9 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/productos", (req, res) => {
     res.json(productos);
     });
-
+app.get("/dias", (req, res) => {
+    res.json(dias);
+    });
 
 app.listen(3002, () => console.log("Server ready on port 3000."));
 
