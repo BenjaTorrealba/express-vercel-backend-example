a8579090de58bae0bdc45a6621eca3d19ea7da92
Benjamin Torrealba <125220991+BenjaTorrealba@users.noreply.github.com>
2025-01-16 13:19:18 +0000

    app D%D

--- api/index.js
+++ api/index.js
@@ -3,6 +3,11 @@ const cors = require("cors");
 const app = express();
 const productos = require("./productos.json");
 const dias = require("./dias.json");
+const atributos =require("./atributos.json")
+const clases =require("./clases.json")
+const habilidades =require("./habilidades.json")
+const razas =require("./razas.json")
+
 app.use(cors()); 
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
@@ -13,7 +18,18 @@ app.get("/productos", (req, res) => {
 app.get("/dias", (req, res) => {
     res.json(dias);
     });
-
+app.get("/atributos"),(req,res) => {
+    res.json(atributos);
+}   
+app.get("/clases"),(req,res) => {
+    res.json(clases);
+}
+app.get("/habilidades"),(req,res) => {
+    res.json(habilidades);
+}
+app.get("/razas"),(req,res) => {
+    res.json(razas);
+}
 app.listen(3002, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
